93e14b97f5af144cf493f56cab4d606ab9631b18
ewilkinsiii <ewilkinsiii@gmail.com>
2023-04-27 08:47:56 -0500

    added experience

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,7 @@ import React, { useRef, useEffect } from "react";
 import profilePic from "../../public/images/profile/developer-pic-2.jpg";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -110,6 +111,7 @@ const about = () => {
           </div>
 
           <Skills />
+          <Experience />
         </Layout>
       </main>
     </>

e0437dc36d47272bb681bed01d3058c4315f6fda
ewilkinsiii <ewilkinsiii@gmail.com>
2023-04-30 11:09:47 -0500

    refactored experience

---
+++ dummy data/experience.js
@@ -0,0 +1,58 @@
+const DUMMY_EXPERIENCE = [
+  {
+    id: "e1",
+    position: "Software Engineer",
+    company: "Google",
+    companyLink: "www.google.com",
+    time: "2022-Present",
+    address: "Mountain View, CA",
+    description:
+      "Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and developing new tools for data analysis and visualization.",
+  },
+  {
+    id: "e2",
+    position: "Intern",
+    company: "Facebook",
+    companyLink: "www.facebook.com",
+    time: "Summer 2021",
+    address: "Menlo Park, CA",
+    description:
+      "Worked on a team responsible for developing a new mobile app feature that allowed users to create and share short-form video content, including designing and implementing a new user interface and developing the backend infrastructure to support the feature.",
+  },
+  {
+    id: "e3",
+    position: "Software Developer",
+    company: "Amazon",
+    companyLink: "www.amazon.com",
+    time: "2020-2021",
+    address: "Seattle, WA",
+    description:
+      "Worked on a team responsible for developing Amazon's mobile app, including implementing new features such as product recommendations and user reviews, and optimizing the app's performance and reliability.",
+  },
+  {
+    id: "e4",
+    position: "Software Developer Intern",
+    company: "Microsoft",
+    companyLink: "www.microsoft.com",
+    time: "Summer 2019",
+    address: "Redmond, WA",
+    description:
+      "Worked on a team responsible for developing new features for Microsoft's Windows operating system, including implementing a new user interface for a system settings panel and optimizing the performance of a core system component.",
+  },
+  {
+    id: "e5",
+    position: "Teaching Assistant",
+    company: "MIT",
+    companyLink: "www.mit.com",
+    time: "Fall 2018",
+    address: "Massachusetts Ave, Cambridge, MA",
+    description:
+      "Assisted in teaching a course on computer programming, held office hours to help students with assignments, and graded exams and assignments.",
+  },
+];
+
+const getAllExperiences = () => {
+  return DUMMY_EXPERIENCE;
+};
+
+export default getAllExperiences;
